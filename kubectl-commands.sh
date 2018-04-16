30b3f6eb202df8629941f1b88e7c832a86a12fae
Steve McQuaid <steve@stevemcquaid.com>
2018-04-16 10:04:27 -0400

    Update

--- kubectl-commands.sh
+++ kubectl-commands.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# https://github.com/stevemcquaid/indycode
+
 mkdir -p $HOME/.kube
 sudo cp -i config $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # To test:
 kubectl get pods --kubeconfig $HOME/.kube/config
+
+NAMESPACE="steve"
+kubectl create namespace $NAMESPACE
+
+kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE
